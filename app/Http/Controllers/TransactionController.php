29f983b38b508ae19b5b74656e4af2e6ccf1fb27
Alex Torres <alejandro.torres@condominio.plus>
2017-03-27 11:30:57 -0600

    created transaction controller

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Transaction::all();
+        return view('transactions.index', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('transactions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'observations' => 'required',
+            'ammount' => 'required',
+            'verified' => 'required'
+        ]);
+
+        Transaction::create($request->all());
+        return redirect()->route('transactions.index')
+                        ->with('success', 'Item created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Transaction $transaction)
+    {
+        $transaction = Transaction::find($transaction);
+        return view('transactions.show', compact('transaction'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Transaction $transaction)
+    {
+        $transaction = Transaction::Find($transaction);
+        return view('transactions.edit', compact('transaction'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Transaction $transaction)
+    {
+        $this->validate($request, [
+            'observations' => 'required',
+            'ammount' => 'required',
+            'verified' => 'required'
+        ]);
+
+        Transaction::find($transaction)->update($request->all());
+        return redirect()->route('transactions.index')
+                        ->with('success', 'Item updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Transaction $transaction)
+    {
+        Transaction::find($transaction)->delete();
+        return redirect()->route('transactions.index')
+                        ->with('success', 'Item deleted successfully');
+    }
+}
