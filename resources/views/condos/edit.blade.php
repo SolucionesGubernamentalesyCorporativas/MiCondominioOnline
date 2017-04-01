dd3adee167ed507ee7a2d1fb90a7c42f2bbf6df2
Alex Torres <alejandro.torres@condominio.plus>
2017-04-01 16:50:41 -0600

    added changes to condo edit view and request

--- resources/views/condos/edit.blade.php
+++ resources/views/condos/edit.blade.php
@@ -10,17 +10,34 @@
         <form class="form-horizontal" role="form" method="POST" action="{{ route('condos.update', $condo->id) }}">
             {{ method_field('PUT') }}
             {{ csrf_field() }}
-            
             <div class="form-group">
-                <strong>Condomino:</strong>
-                <input class="form-control" type="text" name="name">
+                <label for="name" class="col-md-4 control-label">Condomino</label>
+                <div class="col-md-4">
+                    <input id="name" type="text" class="form-control" name="name" placeholder="{{ $condo->name }}" value="{{ old('name') }}">
+                </div>
+                <button type="submit" class="btn btn-primary">Guardar</button>
             </div>
-            <div class="form-group">
-                <strong>Dirección:</strong>
-                <input class="form-control" type="text" name="direction">
-            </div>
-            <button type="submit" class="btn btn-primary">Editar</button>
         </form>
+        <form class="form-horizontal" role="form" method="POST" action="{{ route('condos.update', $condo->id) }}">
+            {{ method_field('PUT') }}
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="direction" class="col-md-4 control-label">Dirección</label>
+                <div class="col-md-4">
+                    <input id="direction" type="text" class="form-control" name="direction" placeholder="{{ $condo->direction }}" value="{{ old('direction') }}">
+                </div>
+                <button type="submit" class="btn btn-primary">Guardar</button>
+            </div>
+        </form>
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
 </div>
 @endsection
