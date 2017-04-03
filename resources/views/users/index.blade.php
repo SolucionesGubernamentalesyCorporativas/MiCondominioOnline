7434d11ff841933344bfef5cb9c5eb59962b3284
Alex Torres <alejandro.torres@condominio.plus>
2017-04-03 14:48:02 -0500

    implemented semantic on user index view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="panel panel-default">
-    <div class="panel-heading clearfix">
-        <h5 style="padding-top: 1.5px;" class="pull-left">Usuarios</h5>
-        <a class="btn btn-default pull-right" href="{{ route('users.create') }}">Añadir usuario</a>
+<div class="ui segments">
+    <div class="ui clearing segment">
+        <div style="padding-top: 3.5px;" class="ui left floated header">Usuarios</div>
+        <button class="ui right floated button" href="{{ route('users.create') }}">Añadir usuario</button>
     </div>
-    <div class="panel-body">
+    <div class="ui segment">
         @if($data)
-            <table class="table">
+            <table class="ui selectable table">
                 <thead>
                     <tr>
-                        <td>Nombre</td>
-                        <td>Apellido</td>
-                        <td>E-mail</td>
-                        <td>Telefono</td>
-                        <td>Opciones</td>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>E-mail</th>
+                        <th>Telefono</th>
+                        <th>Opciones</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -26,13 +26,15 @@
                         <td>{{ $row->email }}</td>
                         <td>{{ $row->phone }}</td>
                         <td>
-                            <a class="btn btn-info" href="{{ route('users.show', $row->id) }}">Info</a>
-                            <a class="btn btn-primary" href="{{ route('users.edit', $row->id) }}">Editar</a>
-                            <form method="POST" action="{{ route('users.destroy', $row->id) }}" style="display: inline;">
-                                {{ method_field('DELETE') }}
-                                {{ csrf_field() }}
-                                <button type="submit" class="btn btn-danger">Borrar</button>
-                            </form>
+                            <div class="ui buttons">
+                                <button class="ui button" href="{{ route('users.show', $row->id) }}">Info</button>
+                                <button class="ui button" href="{{ route('users.edit', $row->id) }}">Editar</button>
+                                <form method="POST" action="{{ route('users.destroy', $row->id) }}" style="display: inline;">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="ui button">Borrar</button>
+                                </form>
+                            </div>
                         </td>
                     </tr>
                 </tbody>
