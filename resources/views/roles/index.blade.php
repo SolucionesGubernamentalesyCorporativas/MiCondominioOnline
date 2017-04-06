f3c81e9894cf5b4b8b48e6ca3de898880d628cb9
Alex Torres <alejandro.torres@condominio.plus>
2017-04-06 14:30:30 -0500

    added changes to user and role index views

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="panel panel-default">
-    <div class="panel-heading clearfix">
-        <h5 style="padding-top: 1.5px;" class="pull-left">Roles</h5>
-        <a class="btn btn-default pull-right" href="{{ route('roles.create') }}">Añadir rol</a>
+<div class="ui segments">
+    <div class="ui clearing segment">
+        <div style="padding-top: 3.5px;" class="ui left floated header">Roles</div>
+        <a class="ui right floated blue button" href="{{ route('roles.create') }}">Añadir rol</a>
     </div>
-    <div class="panel-body">
+    <div class="ui segment">
         @if($data)
-            <table class="table">
+            <table class="ui two column selectable blue table">
                 <thead>
                     <tr>
-                        <td>Rol</td>
-                        <td>Opciones</td>
+                        <th>Rol</th>
+                        <th>Opciones</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -20,13 +20,15 @@
                     <tr>
                         <td>{{ $row->name }}</td>
                         <td>
-                            <a class="btn btn-info" href="{{ route('roles.show', $row->id) }}">Info</a>
-                            <a class="btn btn-primary" href="{{ route('roles.edit', $row->id) }}">Editar</a>
-                            <form method="POST" action="{{ route('roles.destroy', $row->id) }}" style="display: inline;">
-                                {{ method_field('DELETE')}}
-                                {{ csrf_field() }}
-                                <button type="submit" class="btn btn-danger">Borrar</button>
-                            </form>
+                            <div class="ui buttons">
+                                <a class="ui green button" href="{{ route('roles.show', $row->id) }}">Info</a>
+                                <a class="ui blue button" href="{{ route('roles.edit', $row->id) }}">Editar</a>
+                                <form method="POST" action="{{ route('roles.destroy', $row->id) }}" style="display: inline;">
+                                    {{ method_field('DELETE')}}
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="ui red button">Borrar</button>
+                                </form> 
+                            </div>
                         </td>
                     </tr>
                 </tbody>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,46 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="ui segments">
-    <div class="ui clearing segment">
-        <div style="padding-top: 3.5px;" class="ui left floated header">Usuarios</div>
-        <button class="ui right floated button" href="{{ route('users.create') }}">Añadir usuario</button>
+<div class="ui container">
+    <div class="row">
+        <div class="column">
+            <div class="ui clearing blue segment">
+                <div style="padding-top: 3.5px;" class="ui left floated header">Usuarios</div>
+            <a class="ui right floated blue button" href="{{ route('users.create') }}">Añadir usuario</a>
+            </div>
+        </div>
     </div>
-    <div class="ui segment">
-        @if($data)
-            <table class="ui selectable table">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>E-mail</th>
-                        <th>Telefono</th>
-                        <th>Opciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($data as $row)   
-                    <tr>
-                        <td>{{ $row->name }}</td>
-                        <td>{{ $row->lastname }}</td>
-                        <td>{{ $row->email }}</td>
-                        <td>{{ $row->phone }}</td>
-                        <td>
-                            <div class="ui buttons">
-                                <button class="ui button" href="{{ route('users.show', $row->id) }}">Info</button>
-                                <button class="ui button" href="{{ route('users.edit', $row->id) }}">Editar</button>
-                                <form method="POST" action="{{ route('users.destroy', $row->id) }}" style="display: inline;">
-                                    {{ method_field('DELETE') }}
-                                    {{ csrf_field() }}
-                                    <button type="submit" class="ui button">Borrar</button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-                @endforeach
-            </table>
-        @endif
+    <div class="row">
+        <div class="column">
+            @if($data)
+                <table class="ui five column selectable blue table">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th>E-mail</th>
+                            <th>Telefono</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($data as $row)   
+                            <tr>
+                                <td>{{ $row->name }}</td>
+                                <td>{{ $row->lastname }}</td>
+                                <td>{{ $row->email }}</td>
+                                <td>{{ $row->phone }}</td>
+                                <td>
+                                    <div class="ui buttons">
+                                        <a class="ui green button" href="{{ route('users.show', $row->id) }}">Info</a>
+                                        <a class="ui blue button" href="{{ route('users.edit', $row->id) }}">Editar</a>
+                                        <form method="POST" action="{{ route('users.destroy', $row->id) }}" style="display: inline;">
+                                            {{ method_field('DELETE') }}
+                                            {{ csrf_field() }}
+                                            <button type="submit" class="ui red button">Borrar</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    @endforeach
+                </table>
+            @endif
+        </div>
     </div>
 </div>
 @endsection
