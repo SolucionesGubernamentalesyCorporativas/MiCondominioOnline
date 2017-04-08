921da268f1893aac0c1b8d53efabb006bda8f650
Alex Torres <alejandro.torres@condominio.plus>
2017-04-07 21:57:42 -0500

    created index view

---
+++ resources/views/typeofvisitors/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="ui container">
+    <div class="row">
+        <div class="column">
+            <div class="ui clearing blue segment">
+                <div style="position: relative; top: 8px;" class="ui left floated header">Tipo de visitante</div>
+                <a class="ui right floated blue button" href="{{ route('typeofvisitors.create') }}">Añadir tipo de visitante</a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column">
+            @if($data)
+                <table class="ui three column selectable blue table">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Descripción</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($data as $row)   
+                            <tr>
+                                <td>{{ $row->name }}</td>
+                                <td>{{ $row->description }}</td>
+                                <td>
+                                    <div class="ui buttons">
+                                        <a class="ui green button" href="{{ route('typeofvisitors.show', $row->id) }}">Info</a>
+                                        <a class="ui blue button" href="{{ route('typeofvisitors.edit', $row->id) }}">Editar</a>
+                                        <form method="POST" action="{{ route('typeofvisitors.destroy', $row->id) }}" style="display: inline;">
+                                            {{ method_field('DELETE') }}
+                                            {{ csrf_field() }}
+                                            <button type="submit" class="ui red button">Borrar</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    @endforeach
+                </table>
+            @endif
+        </div>
+    </div>
+</div>
+@endsection
