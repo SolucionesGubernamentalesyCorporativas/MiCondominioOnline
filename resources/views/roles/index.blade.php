cf2b19e0f311a56f6c7803f9c3b1b6fcdda54e14
Alex Torres <alejandro.torres@condominio.plus>
2017-04-11 21:05:14 -0500

    added changes to role views

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -42,5 +42,14 @@
             @endif
         </div>
     </div>
+    @if(session('success'))
+        <div class="row">
+            <div class="column">
+                <div class="ui success message">
+                    <p>{{ session('success') }}</p>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
 @endsection
