1920820b94ea514cf9ecddf2ee43c622bcd08d81
Alex Torres <alejandro.torres@condominio.plus>
2017-04-11 23:24:55 -0500

    added changes to condo views

--- resources/views/condos/edit.blade.php
+++ resources/views/condos/edit.blade.php
@@ -1,43 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="panel panel-default">
-    <div class="panel-heading clearfix">
-        <h5 style="padding-top: 1.5px;" class="pull-left">Editar condomino {{ $condo->name }}</h5>
-        <a class="btn btn-default pull-right" href="{{ route('condos.index') }}">Atras</a>
+<div class="ui container">
+    <div class="row">
+        <div class="column">
+            <div class="ui clearing blue segment">
+                <div style="position: relative; top: 8px;" class="ui left floated header">Editar condomino {{ $condo->name }}</div>
+                <a class="ui right floated blue button" href="{{ route('condos.index') }}">Atras</a>
+            </div>
+        </div>
     </div>
-    <div class="panel-body">
-        <form class="form-horizontal" role="form" method="POST" action="{{ route('condos.update', $condo->id) }}">
-            {{ method_field('PUT') }}
-            {{ csrf_field() }}
-            <div class="form-group">
-                <label for="name" class="col-md-4 control-label">Condomino</label>
-                <div class="col-md-4">
-                    <input id="name" type="text" class="form-control" name="name" placeholder="{{ $condo->name }}" value="{{ old('name') }}">
+    <div class="row">
+        <div class="column">
+            <div class="ui blue segment">
+                <div class="ui accordion field">
+                    <div class="item">
+                        <div class="title">
+                            <i class="icon dropdown"></i>
+                            Editar nombre
+                        </div>
+                        <div class="content field">
+                            <form class="ui form error" role="form" method="POST" action="{{ route('condos.update', $condo->id) }}">
+                                {{ method_field('PUT') }}
+                                {{ csrf_field() }}
+                                <div class="eight wide field {{ $errors->has('name') ? 'error' : '' }}">
+                                    <label>Nombre</label>
+                                    <input type="text" name="name" placeholder="{{ $condo->name }}">
+                                    @if ($errors->has('name'))
+                                        <span class="ui error message">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <button class="ui submit blue small button" type="submit">Guardar</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="title">
+                            <i class="icon dropdown"></i>
+                            Editar dirección
+                        </div>
+                        <div class="content field">
+                            <form class="ui form error" role="form" method="POST" action="{{ route('condos.update', $condo->id) }}">
+                                {{ method_field('PUT') }}
+                                {{ csrf_field() }}
+                                <div class="eight wide field {{ $errors->has('direction') ? 'error' : '' }}">
+                                    <label>Dirección</label>
+                                    <input type="text" name="direction" placeholder="{{ $condo->direction }}">
+                                    @if ($errors->has('direction'))
+                                        <span class="ui error message">
+                                            <strong>{{ $errors->first('direction') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <button class="ui submit blue small button" type="submit">Guardar</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Guardar</button>
             </div>
-        </form>
-        <form class="form-horizontal" role="form" method="POST" action="{{ route('condos.update', $condo->id) }}">
-            {{ method_field('PUT') }}
-            {{ csrf_field() }}
-            <div class="form-group">
-                <label for="direction" class="col-md-4 control-label">Dirección</label>
-                <div class="col-md-4">
-                    <input id="direction" type="text" class="form-control" name="direction" placeholder="{{ $condo->direction }}" value="{{ old('direction') }}">
-                </div>
-                <button type="submit" class="btn btn-primary">Guardar</button>
-            </div>
-        </form>
-        @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        </div>
     </div>
 </div>
 @endsection
