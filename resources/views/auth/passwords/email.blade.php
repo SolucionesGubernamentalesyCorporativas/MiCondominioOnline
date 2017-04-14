a0d551be57ff9220f77d334006a844da1332fdf2
Alex Torres <alejandro.torres@condominio.plus>
2017-04-14 10:49:34 -0500

    added semantic ui to password views

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,44 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Restablece Tu Contraseña</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Correo Electrónico</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+<div class="ui centered grid container">
+    <div class="eight wide column">
+        <div class="ui raised segments">
+            <div class="ui segment">
+                <div class="ui header">Restablece tu contraseña</div>
+            </div>
+            <div class="ui blue segment">
+                @if (session('status'))
+                    <div class="row">
+                        <div class="column">
+                            <div class="ui success message">
+                                <p>{{ session('status') }}</p>
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Enviar Enlace De Restablecimiento De Contraseña
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                @endif
+                <form class="ui form error" role="form" method="POST" action="{{ route('password.email') }}">
+                    {{ csrf_field() }}
+                    <div class="field {{ $errors->has('email') ? 'error' : '' }}">
+                        <label>Correo electrónico</label>
+                        <input type="text" name="email" value="{{ old('email') }}">
+                        @if ($errors->has('email'))
+                            <span class="ui error message">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                    <button class="ui submit blue button" type="submit">
+                        Enviar enlace de restablecimiento de contraseña
+                    </button>
+                </form>
             </div>
         </div>
     </div>
