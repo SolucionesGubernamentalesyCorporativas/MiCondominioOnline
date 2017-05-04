5b2b2fe7a348d532fc783c72ced42f7862d0ea0c
Alex Torres <alejandro.torres@condominio.plus>
2017-05-04 08:14:52 -0500

    added incidences views

---
+++ resources/views/Incidences/edit.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="ui container">
+    <div class="row">
+        <div class="column">
+            <div class="ui clearing blue segment">
+                <div style="position: relative; top: 8px;" class="ui left floated header">Editar {{ $incidence->id }}</div>
+                <a class="ui right floated blue button" href="{{ route('incidences.index') }}">
+                    <i class="angle left icon"></i>
+                    Atras
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="ui blue segment">
+                <div class="ui accordion field">
+                    <div class="item">
+                        <div class="title">
+                            <i class="icon dropdown"></i>
+                            Editar descripción
+                        </div>
+                        <div class="content field">
+                            <form class="ui form error" role="form" method="POST" action="{{ route('incidences.update', $incidence->id) }}">
+                                {{ method_field('PUT') }}
+                                {{ csrf_field() }}
+                                <div class="eight wide field {{ $errors->has('description') ? 'error' : '' }}">
+                                    <label>Descripción</label>
+                                    <textarea rows="2" name="description" placeholder="{{ $incidence->description }}">
+                                    @if ($errors->has('description'))
+                                        <span class="ui error message">
+                                            <strong>{{ $errors->first('description') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <button class="ui submit blue small button" type="submit">Guardar</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="title">
+                            <i class="icon dropdown"></i>
+                            Editar tipo de incidencia
+                        </div>
+                        <div class="content field">
+                            <form class="ui form error" role="form" method="POST" action="{{ route('incidences.update', $incidence->id) }}">
+                                {{ method_field('PUT') }}
+                                {{ csrf_field() }}
+                                <div class="eight wide field {{ $errors->has('type_of_incidence_id') ? 'error' : '' }}">
+                                    <label>Tipo de incidencia</label>
+                                    <div class="ui selection dropdown">
+                                        <input type="hidden" name="type_of_incidence_id" value="{{ $incidence->type_of_incidence_id }}">
+                                        <i class="dropdown icon"></i>
+                                        <div class="default text">Selecciona un tipo de incidencia</div>
+                                        <div class="menu">
+                                            @foreach($typeofincidences as $typeofincidence)
+                                                <div class="item" data-value="{{ $typeofincidence->id }}">{{ $typeofincidence->name }}</div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                    @if ($errors->has('type_of_incidence_id'))
+                                        <span class="ui error message">
+                                            <strong>{{ $errors->first('type_of_incidence_id') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <button class="ui submit blue small button" type="submit">Guardar</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="title">
+                            <i class="icon dropdown"></i>
+                            Editar casa
+                        </div>
+                        <div class="content field">
+                            <form class="ui form error" role="form" method="POST" action="{{ route('incidences.update', $incidence->id) }}">
+                                {{ method_field('PUT') }}
+                                {{ csrf_field() }}
+                                <div class="eight wide field {{ $errors->has('estate_id') ? 'error' : '' }}">
+                                    <label>Casa</label>
+                                    <div class="ui selection dropdown">
+                                        <input type="hidden" name="estate_id" value="{{ $incidence->estate_id }}">
+                                        <i class="dropdown icon"></i>
+                                        <div class="default text">Selecciona una casa</div>
+                                        <div class="menu">
+                                            @foreach($estates as $estate)
+                                                <div class="item" data-value="{{ $estate->id }}">{{ $estate->number }}</div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                    @if ($errors->has('estate_id'))
+                                        <span class="ui error message">
+                                            <strong>{{ $errors->first('estate_id') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <button class="ui submit blue small button" type="submit">Guardar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/Incidences/index.blade.php
+++ resources/views/Incidences/index.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="column">
             <div class="ui clearing blue segment">
-                <div style="position: relative; top: 8px;" class="ui left floated header">Recursos</div>
+                <div style="position: relative; top: 8px;" class="ui left floated header">Incidencias</div>
                 <div class="ui right floated blue buttons">
-                    <a class="ui button" href="{{ route('typeofresources.index') }}">Tipos de recurso</a>
-                    <a class="ui button" href="{{ route('resources.create') }}">Añadir recurso</a>
+                    <a class="ui button" href="{{ route('typeofincidences.index') }}">Tipos de incidencia</a>
+                    <a class="ui button" href="{{ route('incidences.create') }}">Añadir incidencia</a>
                 </div>
             </div>
         </div>
@@ -19,21 +19,19 @@
                 <table class="ui three column selectable blue table">
                     <thead>
                         <tr>
-                            <th>Capacidad</th>
-                            <th>Costo</th>
+                            <th>Descripción</th>
                             <th>Opciones</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach($data as $row)
                             <tr>
-                                <td>{{ $row->capacity }}</td>
-                                <td>{{ $row->fee }}</td>
+                                <td>{{ $row->description }}</td>
                                 <td>
                                     <div class="ui small buttons">
-                                        <a class="ui green button" href="{{ route('resources.show', $row->id) }}">Info</a>
-                                        <a class="ui blue button" href="{{ route('resources.edit', $row->id) }}">Editar</a>
-                                        <form method="POST" action="{{ route('resources.destroy', $row->id) }}" style="display: inline;">
+                                        <a class="ui green button" href="{{ route('incidences.show', $row->id) }}">Info</a>
+                                        <a class="ui blue button" href="{{ route('incidences.edit', $row->id) }}">Editar</a>
+                                        <form method="POST" action="{{ route('incidences.destroy', $row->id) }}" style="display: inline;">
                                             {{ method_field('DELETE') }}
                                             {{ csrf_field() }}
                                             <button type="submit" class="ui red button">Borrar</button>
